9611956ffd1bd2d6f7dc7908ad12f5f91af3181d
Tóth Fazekas Balázs <toth-fazekas.balazs.2021i@bankitatabanya.hu>
2025-09-25 14:04:36 +0200

    meghalok de jo

--- src/main/java/edu/banki/totapp/TodoApp.java
+++ src/main/java/edu/banki/totapp/TodoApp.java
@@ -11,15 +11,17 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class TodoApp extends Application {
 
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
         stage.setTitle("Todo List");
 
         TodoController todoController = new TodoController();
-        VBox root = (VBox) todoController.createPane();
+        VBox root = (VBox) todoController.createLayout();
 
         Scene scene = new Scene(root, 400, 300);
         stage.setScene(scene);
--- src/main/java/edu/banki/totapp/TodoController.java
+++ src/main/java/edu/banki/totapp/TodoController.java
@@ -3,13 +3,20 @@ package edu.banki.totapp;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
-public class TodoController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TodoController implements Initializable {
     @FXML
     private ListView<String> todoListView;
     @FXML
@@ -25,25 +32,28 @@ public class TodoController {
 
     private ObservableList<String> todoItems = FXCollections.observableArrayList();
 
-    public VBox createPane() {
+    public VBox createLayout() throws IOException {
 
-        todoListView = new ListView<>(todoItems);
-        todoListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+       // todoListView = new ListView<>(todoItems);
+       // todoListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+//
+       // inputField = new TextField();
+       // inputField.setPromptText("Írj be egy új teendőt");
+       // inputField.setOnAction(e -> addTodo());
+//
+       // addButton = new Button("Hozzáadás");
+       // addButton.setOnAction(e -> addTodo());
+//
+       // deleteButton = new Button("Kijelölt elem törlése");
+       // deleteButton.setOnAction(e -> deleteSelectedTodos());
+       // inputRow = new HBox(10);
+//
+       // inputRow.getChildren().addAll(inputField, addButton);
+       // bottomPanel = new VBox(10, todoListView, inputRow, deleteButton);
+       // bottomPanel.setPadding(new Insets(10));
 
-        inputField = new TextField();
-        inputField.setPromptText("Írj be egy új teendőt");
-        inputField.setOnAction(e -> addTodo());
-
-        addButton = new Button("Hozzáadás");
-        addButton.setOnAction(e -> addTodo());
-
-        deleteButton = new Button("Kijelölt elem törlése");
-        deleteButton.setOnAction(e -> deleteSelectedTodos());
-        inputRow = new HBox(10);
-
-        inputRow.getChildren().addAll(inputField, addButton);
-        bottomPanel = new VBox(10, todoListView, inputRow, deleteButton);
-        bottomPanel.setPadding(new Insets(10));
+        FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("/edu/banki/totapp/todo-view.fxml"));
+        bottomPanel = fxmlloader.load();
 
         return bottomPanel;
     }
@@ -63,5 +73,17 @@ public class TodoController {
             ObservableList<String> itemsToDelete = FXCollections.observableArrayList(selectedItems);
             todoItems.removeAll(itemsToDelete);
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        todoListView.setItems(todoItems);
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        addButton.setOnAction(actionEvent -> {
+            addTodo();
+        });
+        deleteButton.setOnAction(actionEvent -> {
+            deleteSelectedTodos();
+        });
     }
 }
